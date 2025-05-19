91d304c3d21fa92a4065e70fd805c453f1be7f14
wniec <niecwladek@gmail.com>
2025-05-19 16:41:43 +0200

    add torus graph

--- pycellga/main.py
+++ pycellga/main.py
@@ -1,4 +1,5 @@
-from networkx import erdos_renyi_graph
+import networkx as nx
+from networkx import erdos_renyi_graph, grid_graph
 from pycellga.mutation.byte_mutation_random import ByteMutationRandom
 from pycellga.recombination.byte_one_point_crossover import ByteOnePointCrossover
 from pycellga.selection.tournament_selection import TournamentSelection
@@ -8,14 +9,23 @@ from optimizer import graph_cga
 
 if __name__ == "__main__":
     graph = erdos_renyi_graph(n=100, p=0.5)
+    torus_graph = grid_graph(dim=(10, 10), periodic=True)
+    torus_graph_nodes_flattened = {node: i for i, node in enumerate(torus_graph.nodes)}
+    empty_graph = nx.Graph()
+    for edge in torus_graph.edges:
+        empty_graph.add_edge(torus_graph_nodes_flattened[edge[0]], torus_graph_nodes_flattened[edge[1]])
+    # Wierzchołki w grafie torus mają 2 indeksy, więc muszę im pozamieniać nazwy
+    torus_graph = empty_graph
+    n_rows, n_cols = 10, 10
+    n_generations = 100
     result = graph_cga(
-        n_rows=10,
-        n_cols=10,
-        n_gen=100,
+        n_rows=n_rows,
+        n_cols=n_cols,
+        n_gen=n_generations,
         ch_size=10,
         p_crossover=0.9,
         p_mutation=0.2,
-        problem=GraphRastriginProblem(n_var=10, graph=graph, offset=2),
+        problem=GraphRastriginProblem(n_var=10, graph=torus_graph, offset=2),
         selection=TournamentSelection,
         recombination=ByteOnePointCrossover,
         mutation=ByteMutationRandom,
@@ -25,6 +35,7 @@ if __name__ == "__main__":
     # Print the results
     print("Best solution chromosome:", result.chromosome)
     print("Best fitness value:", result.fitness_value)
+    print("Function evaluations:", n_generations * n_rows * n_cols)
 
     # Expected Output:
     # Best solution chromosome: [2.0 for i in range(10)]
